496a6f363e17c7c78d2a72f3774ebef3ba335eec
christ0208 <christopherlimawan@gmail.com>
2020-04-11 00:51:02 +0700

    Edited Subscribe Model to Receive Time

--- model/Subscribe.go
+++ model/Subscribe.go
@@ -1,9 +1,25 @@
 package model
 
-import "time"
+import (
+	"database/sql/driver"
+	"time"
+)
 
 type Subscribe struct {
 	Id string
 	Name string
 	DeletedAt *time.Time
+}
+
+// Scan implements the Scanner interface.
+func (nt *Subscribe) Scan(value interface{}) error {
+	var deletedAt time.Time
+	deletedAt = value.(time.Time)
+	nt.DeletedAt = &deletedAt
+	return nil
+}
+
+// Value implements the driver Valuer interface.
+func (nt Subscribe) Value() (driver.Value, error) {
+	return nt.DeletedAt, nil
 }
