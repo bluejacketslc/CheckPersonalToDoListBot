b1e66b2da0fcc65eeccda5d71daa2acaaac37191
christ0208 <christopherlimawan@gmail.com>
2020-04-11 00:30:13 +0700

    Fixed MySQL Connect: Added DB Name for database connection

--- helpers/mysql_connect.go
+++ helpers/mysql_connect.go
@@ -8,7 +8,7 @@ import (
 )
 
 func CreateConnection() *sql.DB {
-	db, err := sql.Open(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/")
+	db, err := sql.Open(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME"))
 	if err != nil {
 		log.Fatal(err.Error())
 	}
