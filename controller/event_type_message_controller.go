fdb7824dd3545dc4aad87e7bfbf07fd41338812d
christ0208 <christopherlimawan@gmail.com>
2020-04-11 17:47:02 +0700

    Fixed Command Validation for Receive Command Message

--- controller/event_type_message_controller.go
+++ controller/event_type_message_controller.go
@@ -2,23 +2,37 @@ package controller
 
 import (
 	"github.com/line/line-bot-sdk-go/linebot"
+	"strings"
 	"todoreminder/handler"
 )
 
-var commands = map[string] handler.BaseHandler {
-	"/subscribe": handler.SubscribeHandler{},
-	"/unsubscribe": handler.UnsubscribeHandler{},
-	"/add":       handler.AddToDoListHandler{},
-	"/list":      handler.ShowToDoListHandler{},
-	"/remove":    handler.DeleteToDoListHandler{},
-	"/help": handler.HelpToDoListHandler{},
-}
+//var commands = map[string] handler.BaseHandler {
+//	"/subscribe": handler.SubscribeHandler{},
+//	"/unsubscribe": handler.UnsubscribeHandler{},
+//	"/add":       handler.AddToDoListHandler{},
+//	"/list":      handler.ShowToDoListHandler{},
+//	"/remove":    handler.DeleteToDoListHandler{},
+//	"/help": handler.HelpToDoListHandler{},
+//}
 
 type EventTypeMessageController struct {}
 
 func (controller EventTypeMessageController) Execute(bot *linebot.Client, event *linebot.Event) {
 	switch message := event.Message.(type) {
 	case *linebot.TextMessage:
-		commands[message.Text].Handle(bot, event)
+		//commands[message.Text].Handle(bot, event)
+		if strings.Contains(message.Text, "/subscribe") {
+			handler.SubscribeHandler{}.Handle(bot, event)
+		} else if strings.Contains(message.Text, "/unsubscribe") {
+			handler.UnsubscribeHandler{}.Handle(bot, event)
+		} else if strings.Contains(message.Text, "/add") {
+			handler.AddToDoListHandler{}.Handle(bot, event)
+		} else if strings.Contains(message.Text, "/list") {
+			handler.ShowToDoListHandler{}.Handle(bot, event)
+		} else if strings.Contains(message.Text, "/remove") {
+			handler.DeleteToDoListHandler{}.Handle(bot, event)
+		} else if strings.Contains(message.Text, "/help") {
+			handler.HelpToDoListHandler{}.Handle(bot, event)
+		}
 	}
 }
