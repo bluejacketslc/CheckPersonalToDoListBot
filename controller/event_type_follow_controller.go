d7490c585a36b34f9d39d8e709e693f3b9f7f151
christ0208 <christopherlimawan@gmail.com>
2020-04-12 18:22:59 +0700

    Added Welcoming Message

--- controller/event_type_follow_controller.go
+++ controller/event_type_follow_controller.go
@@ -5,10 +5,26 @@ import (
 	"log"
 )
 
+var (
+	welcomingMessage =
+		"Check To Do List Bot\n" +
+		"====================\n" +
+		"Created by CP18-1\n" +
+		"\n" +
+		"Hi, %s.\n" +
+		"This is a LINE bot that will help you for take a note and remind you for every task that you have.\n" +
+		"For basic commands, why don't you try \"/help\" first?\n" +
+		"Enjoy :D\n" +
+		"\n" +
+		"Created with love,\n" +
+		"Christopher Limawan\n" +
+		"Bluejack 18-1"
+)
+
 type EventTypeFollowController struct {}
 
 func (controller EventTypeFollowController) Execute(bot *linebot.Client, event *linebot.Event) {
-	_, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("Hello, New User")).Do()
+	_, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(welcomingMessage)).Do()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
